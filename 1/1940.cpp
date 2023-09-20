703ed669bd1142e2b4bab7a623b17598f572e183
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2023-09-20 15:58:47 +0900

    Day 5

--- 1/1940.cpp
+++ 1/1940.cpp
@@ -8,17 +8,20 @@ int main() {
     cin.tie(NULL);
     cout.tie(NULL);
     cin >> n >> m;
-    for(int i = 0; i < n; i++) {
-        cin >> temp;
-        input.push_back(temp);
-    }
-
-    for(int i = 0; i < n - 1; i++) {
-        for(int j = i+1; j < n; j++) {
-            int sum = input[i] + input[j];
-            if(sum == m) ret++;
+    if(m >200000) cout << 0 << "\n";
+    else {
+        for(int i = 0; i < n; i++) {
+            cin >> temp;
+            input.push_back(temp);
         }
+
+        for(int i = 0; i < n - 1; i++) {
+            for(int j = i+1; j < n; j++) {
+                int sum = input[i] + input[j];
+                if(sum == m) ret++;
+            }
+        }
+        cout << ret <<"\n";
     }
-    cout << ret <<"\n";
     return 0;
 }
