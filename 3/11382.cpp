042086dd48d0d006c6b4220a6acc603ed859472e
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2023-11-06 21:15:06 +0900

    Day 19

---
+++ 3/11382.cpp
@@ -0,0 +1,11 @@
+// 시간이 너무 없어서 오늘은 쉬운문제로 대체합니다...
+#include <bits/stdc++.h>
+using namespace std;
+
+long long a, b, c;
+
+int main() {
+    cin >> a >> b >> c ;
+    cout << a + b + c <<  "\n";
+    return 0;
+}
