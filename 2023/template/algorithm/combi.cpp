95899e0bbef986cfe7f6a1337bedbfaa6df58161
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2025-02-01 14:54:22 +0900

    day1: index 뽑는 조합

--- 2023/template/algorithm/combi.cpp
+++ 2023/template/algorithm/combi.cpp
@@ -4,20 +4,19 @@ using namespace std;
 
 int n, k, a[10];
 
-
-void combi(int cnt, int start){ 
-    if(cnt == k){
-        for(int i = 0; i < k; i++) {
-            cout <<  a[i] << " ";
-        } 
+void combi(int cnt, int start) {
+    if (cnt == k) {
+        for (int i = 0; i < k; i++) {
+            cout << a[i] << " ";
+        }
         cout << '\n';
-        return; 
+        return;
     }
-    for(int i = start; i <= n; i++){ 
-            a[cnt] = i;
-            combi(cnt + 1, i + 1);
+    for (int i = start; i <= n; i++) {
+        a[cnt] = i;
+        combi(cnt + 1, i + 1);
     }
-    return; 
+    return;
 }
 
 int main() {
@@ -29,3 +28,33 @@ int main() {
     return 0;
 }
 
+//  인덱스 뽑는 조합
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, k, a[10];
+
+void combi(int cnt, int start) {
+    if (cnt == k) {
+        for (int i = 0; i < k; i++) {
+            cout << a[i] << " ";
+        }
+        cout << '\n';
+        return;
+    }
+    for (int i = start; i < n; i++) {
+        a[cnt] = i;
+        combi(cnt + 1, i + 1);
+    }
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n >> k;
+    // cout << n << k;
+    combi(0, 0);
+    return 0;
+}
